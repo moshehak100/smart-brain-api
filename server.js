71506a80df8509ea3e0c2473d4d1f46f1c6d37d4
Moshe <moshehak10@gmail.com>
2021-04-14 16:53:55 +0300

    serve added

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
+console.log("DB address: ",process.env.DATABASE_URL);
 app.listen(process.env.PORT, ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
 })
